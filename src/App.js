5ce59cd88c412963f3891ffc01f6ca5b1573db9e
efe3535 <a.efe.akyazi@gmail.com>
2022-01-23 14:19:29 +0300

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { Helmet } from 'react-helmet'
+import { createElement, Component } from 'react';
+import reactDom from 'react-dom';
+
+class HelloReact extends Component {
+   render() {
+       return (
+		<center>
+          <h1 className='clickedshow'>Sonunda bir adim daha yakinim!</h1>
+       	</center>
+	   );
+   }
+}
+
+class MainPage extends Component {
+	constructor(props) {
+		super(props);
+    	this.state = {isToggleOn: true};
+	}
+
+	dashboard() {
+		reactDom.render(
+				<div>
+					<MainPage/>
+					<HelloReact/>
+				</div>, 
+				document.getElementById('root')
+		)
+
+	}
+	
+	render() {
+		return(
+			<div className="App">
+	  			<Helmet>
+	  				<style>{"body { background-color: #121212; }"}</style>
+	  			</Helmet>	
+	  
+	  			<h1 className="goatheader">Welcome Goat!</h1>
+	 			<center>
+		  			<button className='goatdashboard' onClick={this.dashboard}>GoatToken Request</button> 
+				</center>
+			</div> 
+		);	
+	}
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	reactDom.render(<MainPage/>, document.getElementById('root'));
+	
 }
 
 export default App;
