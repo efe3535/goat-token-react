f00671550684a2b6b9829e48e7b533e8bee467b5
efe3535 <a.efe.akyazi@gmail.com>
2022-01-24 11:32:17 +0300

    Sends email now

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
 import './App.css';
-import { Helmet } from 'react-helmet'
-import { createElement, Component } from 'react';
+import { Helmet } from 'react-helmet';
+import { Component } from 'react';
 import reactDom from 'react-dom';
+import emailjs from 'emailjs-com';
+const mail = require('./mail.json');
 
 class HelloReact extends Component {
-   render() {
-       return (
+	constructor(props) {
+		super(props);
+		this.state = {isToggleOn: true};
+		this.sendMail = this.sendMail.bind(this);
+		this.amount = '';
+		this.reason =  '';
+		this.name = '';
+	}
+	
+	sendMail() {
+		if(this.name,this.amount,this.reason!="")
+		{	
+			alert(`Name: ${this.name}\nAmount: ${this.amount}\nReason: ${this.reason}`);
+			const sendEmail = () => {
+				var templateParams = {
+					from_name: `${this.name}`,
+					to_name: "Coinbro",
+					message: `Name: ${this.name}\nAmount: ${this.amount}\nReason: ${this.reason}`,
+				}
+
+    			emailjs.send(mail.serviceid, mail.templateid, templateParams , mail.userid)
+      			.then((result) => {
+          			console.log(result.text);
+      			}, (error) => {
+          			console.log(error.text);
+      			});
+  			};
+
+			sendEmail();
+
+		}
+	}
+	
+	render() {
+	  return (
 		<center>
-          <h1 className='clickedshow'>Sonunda bir adim daha yakinim!</h1>
-       	</center>
+		   	<input type="text" onChange={event=>{this.name = event.target.value}} className="name" placeholder='Your name?'/>
+		   	<br/>
+		   	<input type="text" onChange={event=>{this.amount = event.target.value}} className="amount" placeholder='How much?'/>
+		   	<br/>
+		   	<input type="text" onChange={event=>{this.reason = event.target.value}} className="reason" placeholder='Reason?'/>
+			<br/>
+		  	<button className='submit' onClick={this.sendMail}>Send Request</button>
+		</center>
 	   );
    }
 }
